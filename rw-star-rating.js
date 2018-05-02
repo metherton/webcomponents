cc673f6a444c75cc0962e7ba721ce411e2d9e6ed
metherton <martinetherton63@gmail.com>
2018-05-02 23:26:06 +0200

    adding a value property to star rating

--- rw-star-rating.js
+++ rw-star-rating.js
@@ -12,7 +12,20 @@ class RwStarRating extends HTMLElement {
         this._$bottom = null;
         // Data
         this._disabled = false;
+        this._value = 0;
+
     }
+
+    set value(value) {
+        if (this._value === value) return;
+        this._value = value;
+        this._render();
+    }
+
+    get value() {
+        return this._value;
+    }
+
     connectedCallback() {
         this._root.innerHTML = `
         <style>
@@ -32,6 +45,7 @@ class RwStarRating extends HTMLElement {
                 margin: 0 auto;
                 position: relative;
                 padding: 0;
+                cursor: pointer;
             }
             .container .top {
                 color: #e7bd06;
@@ -80,7 +94,14 @@ class RwStarRating extends HTMLElement {
             </div>
         </div>
         `;
-        this._disabled = (this.getAttribute("disabled") !== null)
+        this._disabled = (this.getAttribute("disabled") !== null);
+        this._$top = this._root.querySelector(".top");
+    }
+
+    _render() {
+        if (this._$top !== null) {
+            this._$top.style.width = ((this._value * 10) * 2) + "%";
+        }
     }
 
     static get observedAttributes() {
