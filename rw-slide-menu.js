ce616d310aafd99db64c3a64b9e7dd5e89e9593f
metherton <martinetherton63@gmail.com>
2018-05-03 23:45:47 +0200

    adding buttons to open and close the menu

--- rw-slide-menu.js
+++ rw-slide-menu.js
@@ -72,14 +72,23 @@ class RwSlideMenu extends HTMLElement {
                     background-color: #0D152D;
                     color: white;
                 }
+                :host([backdrop="false"]) .frame.open {
+                    pointer-events: none;
+                    background-color: inherit;
+                }
+                :host([backdrop="false"]) .frame.open .container {
+                    pointer-events: auto;
+                }
+
+                
             </style>
-            <div class="frame">
+            <div class="frame" data-close="true">
                 <nav class="container">
                     <div class="title">
                         <div class="title-content">
                             Menu
                         </div>
-                        <a class="close">&#10006;</a>
+                        <a class="close" data-close="true">&#10006;</a>
                     </div>
                     <div class="content">
                         <a href="#">Menu Item one</a>
@@ -87,7 +96,38 @@ class RwSlideMenu extends HTMLElement {
                 </nav>
             </div>
         `;
+        this._$frame = this._root.querySelector(".frame");
+        this._$frame.addEventListener("click", (event) => {
+           if (event.target.dataset.close === "true") {
+                this.open = false;
+            }
+        });
     }
+
+    _render() {
+        if (this._$frame !== null) {
+            if (this._open === true) {
+                this._$frame.classList.add("open");
+                this.dispatchEvent(new CustomEvent("menu-opened"));
+            } else {
+                this._$frame.classList.remove("open");
+                this.dispatchEvent(new CustomEvent("menu-closed"));
+
+            }
+        }
+    }
+
+    set open(value) {
+        const result = (value === true);
+        if (this._open === result) return;
+        this._open = result;
+        this._render();
+    }
+
+    get open() {
+        return this._open;
+    }
+
 }
 
 window.customElements.define('rw-slide-menu', RwSlideMenu);
